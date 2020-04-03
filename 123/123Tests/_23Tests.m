f3113b83594502da304ba1391ee0a7b8f420d57e
Smallyanlei <1084116072@qq.com>
2020-04-03 10:48:25 +0800

    第一次

---
+++ 123/123Tests/_23Tests.m
@@ -0,0 +1,37 @@
+//
+//  _23Tests.m
+//  123Tests
+//
+//  Created by ReadyForSale on 4/3/20.
+//  Copyright © 2020 ReadFo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _23Tests : XCTestCase
+
+@end
+
+@implementation _23Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
